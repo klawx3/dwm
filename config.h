1c459d199888c8b9da378c01db3534ff679ff641
klawx3 <klawx3@gmail.com>
2020-11-07 20:54:22 -0300

    added shorcuts

--- config.h
+++ config.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Brave-browser",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -67,12 +67,24 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browser[]  = { "brave", NULL };
+static const char *discord[]  = { "discord", NULL };
+static const char *spotify[]  = { "spotify", NULL };
+static const char *filemanager[]  = { "pcmanfm", NULL };
+
+
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+
+	{ MODKEY,                       XK_F1,     spawn,          {.v = filemanager} },
+	{ MODKEY,                       XK_F8,     spawn,          {.v = discord} },
+	{ MODKEY,                       XK_F9,     spawn,          {.v = spotify} },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browser } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_v,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 //	{ MODKEY,                       XK_v,      shiftview,      {.i = +1 } },
